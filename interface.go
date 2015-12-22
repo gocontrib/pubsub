263c3ec89efb78102ea995d0eeeff4207ad1360c
sergeyt <stodyshev@gmail.com>
2015-12-22 22:24:46 +0600

    comments

--- interface.go
+++ interface.go
@@ -2,14 +2,20 @@ package pubsub
 
 // Hub interface of pubsub system.
 type Hub interface {
+	// Publish sends input message to specified channels.
 	Publish(channels []string, msg interface{})
+	// Subscribe opens channel to listen specified channels.
 	Subscribe(channels []string) (Channel, error)
+	// Close stops the pubsub hub.
 	Close() error
 }
 
 // Channel to listen pubsub events.
 type Channel interface {
+	// Read returns channel to receive events.
 	Read() <-chan interface{}
+	// Close stops listening underlying pubsub channels.
 	Close() error
+	// CloseNotify returns channel to receive event when this channel is closed.
 	CloseNotify() <-chan bool
 }
