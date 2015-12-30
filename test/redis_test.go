cfe16345f2b315944e05c4f29fbad17337ca01ef
sergeyt <stodyshev@gmail.com>
2015-12-31 03:22:13 +0600

    fixing redis test

--- test/redis_test.go
+++ test/redis_test.go
@@ -1,13 +1,18 @@
 package test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/gocontrib/pubsub/redis"
 )
 
 func TestRedis_Basic(t *testing.T) {
-	hub, err := redis.Open("tcp://127.0.0.1:6379/11")
+	url := os.Getenv("REDIS_URL")
+	if len(url) == 0 {
+		url = "tcp://127.0.0.1:6379/11"
+	}
+	hub, err := redis.Open(url)
 	ok(t, "Open", err)
 	verifyBasicAPI(t, hub)
 }
