a3ba1ba2635e7612d320003abc8cd2164dc7c695
sergeyt <stodyshev@gmail.com>
2018-10-02 22:41:17 +0700

    define common event

--- interface.go
+++ interface.go
@@ -1,5 +1,7 @@
 package pubsub
 
+import "time"
+
 // Hub interface of pubsub system.
 type Hub interface {
 	// Publish sends input message to specified channels.
@@ -18,4 +20,18 @@ type Channel interface {
 	Close() error
 	// CloseNotify returns channel to receive event when this channel is closed.
 	CloseNotify() <-chan bool
+}
+
+// Event defines abstract event usually about HTTP update
+type Event struct {
+	ID           string      `json:"id,omitempty"`     // event id
+	Action       string      `json:"action,omitempty"` // http method or specific action
+	Method       string      `json:"method,omitempty"` // http method
+	URL          string      `json:"url,omitempty"`
+	ResourceID   string      `json:"resource_id,omitempty"`   // resource id
+	ResourceType string      `json:"resource_type,omitempty"` // resource type
+	Payload      interface{} `json:"payload,omitempty"`       // input payload
+	CreatedBy    string      `json:"created_by,omitempty"`
+	CreatedAt    time.Time   `json:"created_at,omitempty"`
+	Result       interface{} `json:"result,omitempty"` // any result of mutation
 }
